98c017c0ebd54c3b61e5ee9dc32aebe522b9c0aa
Yogita Goyal <yogitagoel2006@gmail.com>
2025-03-13 01:07:45 +0530

    Create SafeMathTester.sol

---
+++ SafeMathTester.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract SafemathTester{
+
+    uint8 public bigNumber=255;
+
+    function add() public{
+        bigNumber=bigNumber+1;
+    }
+
+}
